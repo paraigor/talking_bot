76a4348d0f86506ece8f1bfe69420b1c0bb8d26d
paraigor <103633539+paraigor@users.noreply.github.com>
2024-12-28 20:12:34 +0300

    Setup DialogFlow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from environs import Env
+from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import (
     CallbackContext,
@@ -10,37 +11,36 @@ from telegram.ext import (
     Updater,
 )
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-
 logger = logging.getLogger(__name__)
-
-env = Env()
-env.read_env()
-TG_BOT_TOKEN = env.str("TG_BOT_TOKEN")
 
 
 def start(update: Update, context: CallbackContext):
-    pass
+    update.message.reply_text("Здравствуйте!")
 
 
-def send_message(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    message = update.message.text
-    context.bot.send_message(
-        text=message,
-        chat_id=chat_id,
+def echo(update: Update, context: CallbackContext):
+    text_input = dialogflow.TextInput(
+        text=update.message.text, language_code="ru"
     )
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    update.message.reply_text(response.query_result.fulfillment_text)
 
 
 def main():
-    updater = Updater(TG_BOT_TOKEN, use_context=True)
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
+
+    updater = Updater(tg_bot_token, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, send_message)
+        MessageHandler(Filters.text & ~Filters.command, echo)
     )
 
     updater.start_polling()
@@ -48,4 +48,11 @@ def main():
 
 
 if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+    tg_bot_token = env.str("TG_BOT_TOKEN")
+    google_project_id = env.str("GOOGLE_PROJECT_ID")
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(google_project_id, tg_bot_token)
     main()
