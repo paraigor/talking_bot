44d9c35c4bfad8ce671b3e99f81bf44260edb9ef
paraigor <103633539+paraigor@users.noreply.github.com>
2024-12-29 14:01:36 +0300

    Add fallback check for VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -16,11 +16,12 @@ def echo(event, vk_api):
         request={"session": session, "query_input": query_input}
     )
 
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=response.query_result.fulfillment_text,
-        random_id=random.randint(1,1000)
-    )
+    if not response.query_result.intent.is_fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response.query_result.fulfillment_text,
+            random_id=random.randint(1,1000)
+        )
 
 
 if __name__ == "__main__":
