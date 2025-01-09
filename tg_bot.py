8d3dfa20487fbc1f55179f8198381b63c6dbc96a
paraigor <103633539+paraigor@users.noreply.github.com>
2025-01-09 20:34:02 +0300

    Move log handler class to common file

--- tg_bot.py
+++ tg_bot.py
@@ -10,18 +10,9 @@ from telegram.ext import (
     Updater,
 )
 
+from log_handler import TgLogHandler
+
 logger = logging.getLogger(__file__)
-
-
-class TgLogHandler(logging.Handler):
-    def __init__(self, tg_bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.tg_bot = tg_bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def start(update: Update, context):
--- vk_bot.py
+++ vk_bot.py
@@ -7,18 +7,9 @@ from environs import Env
 from google.cloud import dialogflow
 from vk_api.longpoll import VkEventType, VkLongPoll
 
+from log_handler import TgLogHandler
+
 logger = logging.getLogger(__file__)
-
-
-class TgLogHandler(logging.Handler):
-    def __init__(self, tg_bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.tg_bot = tg_bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def echo(event, vk_api):
